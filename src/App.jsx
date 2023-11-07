22ca4f8bf9acd4ef7ccd531f90f10342db563984
ioangheraszim <gijohny17@gmail.com>
2023-11-07 19:15:56 +0000

    started building my portfolio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+   <main>
+    <h1>PORTFOLIO</h1>   
+   </main>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <header>Navbar</header>
+  )
+}
+
+export default Navbar
