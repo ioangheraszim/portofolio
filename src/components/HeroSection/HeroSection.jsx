d7e32216e5f83ab98537ff76aeb6f6c48c283645
ioangheraszim <gijohny17@gmail.com>
2023-11-10 00:02:34 +0000

    finished navbar starting on hero section

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <section>
+        <h1>Hello World</h1>
+    </section>
+  )
+}
+
+export default HeroSection
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Navbar.scss"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
 function Navbar() {
+
+  const [toggleNav, setToggleNav] = useState(false);
+
+  const openNav = () => {
+    setToggleNav(true)
+  }
+
+  const closeNav = () => {
+    setToggleNav(false)
+  }
+
   return (
     <header>
-      <nav>
+      <nav className='container navbar'>
         <div>
-        <p>MrEone</p>
+          <a href=''>MrEone</a>
         </div>
-        <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">Skills</a></li>
-          <li><a href="">Projects</a></li>
-          <li><a href="">Contact</a></li>
-        </ul>
-        <div>
-          <p></p>
-          <p></p>
+        <div className={`nav-container ${toggleNav ? "active" : ""}`}>
+          <button className='nav-button close' onClick={closeNav}><FontAwesomeIcon icon={faX} /></button>
+          <ul className='nav-list'>
+            <li onClick={closeNav} className={`${toggleNav ? 'from-right active' : 'from-right'}`}><a href="">Home</a></li>
+            <li onClick={closeNav} className={toggleNav ? 'from-left active' : 'from-left'}><a href="">Skills</a></li>
+            <li onClick={closeNav} className={`${toggleNav ? 'from-right active' : 'from-right'}`}><a href="">Projects</a></li>
+            <li onClick={closeNav} className={toggleNav ? 'from-left active' : 'from-left'}><a href="">Contact</a></li>
+          </ul>
+        </div>
+        <div className='social-icons'>
+          <a target='_blank' href=''><FontAwesomeIcon icon={faGithub} /></a>
+          <a target='_blank' href=''><FontAwesomeIcon icon={faLinkedinIn} /></a>
+          <button className='nav-button open' onClick={openNav} aria-label='navbar open'><FontAwesomeIcon icon={faBars} /></button>
         </div>
       </nav>
     </header>
